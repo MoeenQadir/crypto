2d3e56913514af0ce09e372976cffa8e7adddb65
MoeenQadir <myselfxdeveloper@gmail.com>
2024-03-12 20:57:22 +0500

    complete template

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Banner from './components/Banner/index';
-import Companies from './components/Companies/index';
 import Work from './components/Work/index';
 import Table from './components/Table/index';
 import Features from './components/Features/index';
@@ -12,7 +11,6 @@ export default function Home() {
   return (
     <main>
       <Banner />
-      <Companies />
       <Work />
       <Table />
       <Features />
